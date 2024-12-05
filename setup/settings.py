23b3e3a4f34019eac52fa131d74b15bbf68ac911
João Vitor de Oliveira <vitor-oliveira001@hotmail.com>
2024-12-05 14:16:28 -0300

    CSS, STATIC CONFIG, URL CONFIG

--- setup/settings.py
+++ setup/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# Diretório onde os arquivos estáticos serão armazenados após o comando collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
